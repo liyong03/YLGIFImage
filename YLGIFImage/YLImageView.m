bb5214dca828d107b63a3d3958d9c9068511974e
luhuaquan <luhuaquan@zhiketong.cn>
2020-12-19 08:52:40 +0800

    修复  iOS14 无法正常显示图片
    参考自： https://www.jianshu.com/p/f519990edeca

--- YLGIFImage/YLImageView.m
+++ YLGIFImage/YLImageView.m
@@ -168,8 +168,13 @@ const NSTimeInterval kMaxTimeStep = 1; // note: To avoid spiral-o-death
         return;
     }
     //NSLog(@"display index: %luu", (unsigned long)self.currentFrameIndex);
-    if(self.currentFrame && ![self.currentFrame isKindOfClass:[NSNull class]])
+    if(self.currentFrame && ![self.currentFrame isKindOfClass:[NSNull class]]){
         layer.contents = (__bridge id)([self.currentFrame CGImage]);
+    }else{
+        if (@available(iOS 14.0, *)) {
+            [super displayLayer:layer];
+        }
+    }
 }
 
 - (void)didMoveToWindow
